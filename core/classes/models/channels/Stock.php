2606b1e485bdeacdd140659ea1a6cac5234cff60
Michael Berry <berryintlonline@gmail.com>
2017-07-12 16:11:17 -0600

    Refactor Stock

--- core/classes/models/channels/Stock.php
+++ core/classes/models/channels/Stock.php
@@ -2,41 +2,39 @@
 
 namespace models\channels;
 
-
-
 use models\ModelDB as MDB;
 
 class Stock
 {
 
-    public static function getIdFromSKUId($sku_id)
+    public static function getIdBySkuId($skuID)
     {
         $sql = "SELECT id 
                 FROM stock 
                 WHERE sku_id = :sku_id";
         $queryParams = [
-            ':sku_id' => $sku_id
+            ':sku_id' => $skuID
         ];
         return MDB::query($sql, $queryParams, 'fetchColumn');
     }
 
-    public static function save($sku_id, $condition_id = null, $uofm = 1)
+    public static function save($skuID, $conditionID = null, $uofmID = 1)
     {
         $sql = "INSERT INTO stock (sku_id, condition_id, uofm_id) 
                 VALUES (:sku_id, :condition_id, :uofm_id)";
         $queryParams = [
-            ":sku_id" => $sku_id,
-            ":condition_id" => $condition_id,
-            ":uofm_id" => 1
+            ":sku_id" => $skuID,
+            ":condition_id" => $conditionID,
+            ":uofm_id" => $uofmID
         ];
         return MDB::query($sql, $queryParams, 'id');
     }
 
-    public static function searchOrInsert($sku_id, $condition_id = null, $uofm = 1)
+    public static function searchOrInsert($skuID, $conditionID = null, $uofm = 1)
     {
-        $stock_id = Stock::getIdFromSKUId($sku_id);
+        $stock_id = Stock::getIdBySkuId($skuID);
         if (empty($stock_id)) {
-            $stock_id = Stock::save($sku_id, $condition_id, $uofm);
+            $stock_id = Stock::save($skuID, $conditionID, $uofm);
         }
         return $stock_id;
     }

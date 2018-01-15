01966041886dd4c5dfacd497c25054dcee43eb12
Michael Berry <berryintlonline@gmail.com>
2018-01-15 08:59:33 -0700

    Another attempt to loop WalmartItems

--- core/classes/Walmart/WalmartInventory.php
+++ core/classes/Walmart/WalmartInventory.php
@@ -29,12 +29,17 @@ class WalmartInventory extends Walmart
         $wmItems = $this->configure();
         $items = [];
         $i = 0;
-//        for($i = 0; $i < 21; $i+=20) {
-            $items[] = $wmItems->list([
-                'offset' => $i
-            ]);
-//        }
+        for($i = 0; $i < 21; $i+=20) {
+            $items[] = $this->listItems($wmItems, $i);
+        }
         return $items;
+    }
+
+    public function listItems(WalmartItem $wmItems, $offset)
+    {
+        return $wmItems->list([
+            'offset' => $offset
+        ]);
     }
 
     public function updatePrice($sku, $price)

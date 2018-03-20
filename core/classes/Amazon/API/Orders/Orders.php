974916a496467bc89e37fe76364f6dc004b7374f
Michael Berry <berryintlonline@gmail.com>
2018-03-20 16:05:54 -0600

    Begin adding Orders API datatypes

--- core/classes/Amazon/API/Orders/Orders.php
+++ core/classes/Amazon/API/Orders/Orders.php
@@ -18,5 +18,35 @@ class Orders
     protected static $versionDate = "2013-09-01";
     private static $overviewUrl = "http://docs.developer.amazonservices.com/en_US/orders-2013-09-01/Orders_Overview.html";
     private static $libraryUpdateUrl = "http://docs.developer.amazonservices.com/en_US/orders-2013-09-01/Orders_ClientLibraries.html";
+    protected static $dataTypes = [
+        "Address" => [
+            "Name" => [
+                "required"
+            ],
+            "AddressLine1",
+            "AddressLine2",
+            "AddressLine3",
+            "City",
+            "County",
+            "District",
+            "StateOrRegion",
+            "PostalCode",
+            "CountryCode" => [
+                "maximumLength" => 2
+            ],
+            "Phone",
+            "AddressType" => [
+                "validWith" => [
+                    "Commercial",
+                    "Residential"
+                ]
+            ]
+        ],
+        "BuyerCustomizedInfo" => [
+            "CustomizedURL" => [
+                "required"
+            ]
+        ]
+    ];
 
 }

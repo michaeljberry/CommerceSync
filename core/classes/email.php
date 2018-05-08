0a2b32caeeb9a24a79f7868a3a3aacdafcd75175
Michael Berry <berryintlonline@gmail.com>
2018-05-08 15:11:47 -0600

    Fix stray merge code

--- core/classes/email.php
+++ core/classes/email.php
@@ -5,10 +5,6 @@ $mail->isSMTP();
 $mail->Host = getenv("EMAIL_HOST");
 $mail->Port = getenv("EMAIL_PORT");
 //$mail->SMTPAuth = true;
-<<<<<<< HEAD
 $mail->Username = getenv(" EMAIL_USER");
-=======
-$mail->Username = getenv("EMAIL_USER");
->>>>>>> 7bdafca1277f31c5e0f31f8209ec60e80446f973
 $mail->Password = getenv("EMAIL_PASSWORD");
 $mail->SMTPDebug = 1;

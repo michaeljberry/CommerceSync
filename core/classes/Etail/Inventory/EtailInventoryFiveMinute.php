2a4cfb939339cd44486df24c3435dfb0bb3f57aa
Michael Berry <berryintlonline@gmail.com>
2018-05-14 16:07:57 -0600

    Fix interval

--- core/classes/Etail/Inventory/EtailInventoryFiveMinute.php
+++ core/classes/Etail/Inventory/EtailInventoryFiveMinute.php
@@ -7,7 +7,7 @@ use CSV\CSV;
 class EtailInventoryFiveMinute extends EtailInventoryUpdate
 {
 
-    protected $interval = 5;
+    protected $interval = 15;
 
     public function __construct()
     {
